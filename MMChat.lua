cea395e7ac017923b6a255222e964696a1e88368
Zig Zichterman <ziggr@ziggr.com>
2016-10-17 20:41:07 -0700

    Cleaned up
    No more auto-price-check or attempts to write to chat.

--- MMChat.lua
+++ MMChat.lua
@@ -4,27 +4,23 @@ local MMChat = {}
 MMChat.name            = "MMChat"
 MMChat.version         = "2.6.1"
 MMChat.savedVarVersion = 1
-MMChat.NAME_BANK       = "bank"
-MMChat.NAME_CRAFT_BAG  = "craft bag"
-MMChat.char_index      = nil
 MMChat.default = {
-    bag = {}
-}
-MMChat.channel_id_enabled = {
-  [CHAT_CHANNEL_WHISPER] = true
-, [CHAT_CHANNEL_GUILD_3] = true
-, [CHAT_CHANNEL_GUILD_4] = true
---, [CHAT_CHANNEL_GUILD_5] = true
-, [CHAT_CHANNEL_OFFICER_3] = true
-, [CHAT_CHANNEL_OFFICER_4] = true
---, [CHAT_CHANNEL_OFFICER_5] = true
+    channel_enabled = {
+      [CHAT_CHANNEL_WHISPER] = true
+    , [CHAT_CHANNEL_GUILD_1] = false, [CHAT_CHANNEL_OFFICER_1] = false
+    , [CHAT_CHANNEL_GUILD_2] = false, [CHAT_CHANNEL_OFFICER_2] = false
+    , [CHAT_CHANNEL_GUILD_3] = true,  [CHAT_CHANNEL_OFFICER_3] = true
+    , [CHAT_CHANNEL_GUILD_4] = true,  [CHAT_CHANNEL_OFFICER_4] = true
+    , [CHAT_CHANNEL_GUILD_5] = false, [CHAT_CHANNEL_OFFICER_5] = false
+    }
 }
 MMChat.text_queue = {}
 
 MMChat.LINK_PATTERN   = '|H%d:item:[0-9:]+|h[^|]*|h'
 MMChat.LINK_PATTERN_N = '([%d+x]*) ?('..MMChat.LINK_PATTERN..')'
 
--- Chat colors
+-- Chat Colors ---------------------------------------------------------------
+
 MMChat.GREY = "999999"
 
 function MMChat.color(color, text)
@@ -40,15 +36,23 @@ function MMChat.round(f)
     return math.floor(0.5+f)
 end
 
+-- Number/String conversion --------------------------------------------------
+
 -- Return commafied integer number, or "?" if nil.
 function MMChat.ToMoney(x)
     if not x then return "?" end
     return ZO_CurrencyControl_FormatCurrency(MMChat.round(x), false)
 end
 
+-- Convert "100x" to integer 100. Convert empty (no multiplier) to 1.
+function MMChat:ToInt(s)
+    if not s or s == "" then return 1 end
+    local nox = string.gsub(s,"x", "")
+    return tonumber(nox)
+end
 
+-- Item ======================================================================
 
--- Item ----------------------------------------------------------------------
 local Item = {}
 function Item:FromLinkCt(link, ct)
     local o = { total_value = 0
@@ -87,36 +91,14 @@ Item.BIND_STRING = {
 , [BIND_TYPE_UNSET]              = "unset"
 }
 
--- Return Master Merchant's "stats to chat" text.
---
--- Basically copied straight from Master Merchant's own
--- MasterMerchant:onItemActionLinkStatsLink()
-function Item:PriceCheckText()
-  local tipLine, days = MasterMerchant:itemPriceTip(self.link, true)
-  if not tipLine then
-    if days == 10000 then
-      tipLine = GetString(MM_TIP_FORMAT_NONE)
-    else
-      tipLine = string.format(GetString(MM_TIP_FORMAT_NONE_RANGE), days)
-    end
-  end
-  if tipLine then
-    tipLine = string.gsub(tipLine, 'M.M.', 'MM')
-    local ChatEditControl = CHAT_SYSTEM.textEntry.editControl
-    if (not ChatEditControl:HasFocus()) then StartChatInput() end
-    local itemText = string.gsub(self.link, '|H0', '|H1')
-    return(MasterMerchant.concat(tipLine, GetString(MM_TIP_FOR), itemText))
-  end
-  return ""
-end
-
 function Item:BoundHow()
     if not IsItemLinkBound(self.link) then return "" end
     local bt = GetItemLinkBindType(self.link)
     return Item.BIND_STRING[bt]
 end
 
--- ChatEvent --------------------------------------------------------------------
+-- ChatEvent =================================================================
+
 local ChatEvent = {}
 function ChatEvent:New(channel_id, from, text)
     local o = { channel_id  = channel_id
@@ -128,10 +110,11 @@ function ChatEvent:New(channel_id, from, text)
     return o
 end
 
--- MMChat --------------------------------------------------------------------
+-- MMChat ====================================================================
+
 -- Might some day go dynamic, but for now...
 function MMChat:IsEnabledForChannelID(channel_id)
-    return MMChat.channel_id_enabled[channel_id]
+    return MMChat.savedVariables.channel_enabled[channel_id]
 end
 
 -- Don't report on our own MM reports!
@@ -156,25 +139,18 @@ function MMChat:ContainsLink(text)
     return string.match(text, MMChat.LINK_PATTERN)
 end
 
--- Convert "100x" to integer 100. Convert empty (no multiplier) to 1.
-function MMChat:ToInt(s)
-    if not s or s == "" then return 1 end
-    local nox = string.gsub(s,"x", "")
-    return tonumber(nox)
-end
-
 -- Donated by @astraea360: 10x|H1:item:54181:34:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h 100x|H1:item:54180:33:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h 200x|H1:item:54179:32:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h 200x|H1:item:54178:31:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h
 -- (Flash Auction, 15 seconds) Donated by @dot_hackza: |H1:item:84705:362:50:0:0:0:0:0:0:0:0:0:0:0:0:1:0:0:0:0:0|h|h
 
--- Given a string "100xRawhide 50xHemming Dreugh Wax"
+-- Given a string "100xRawhide 50xHemming Jute"
 -- return a list of Items
---      (100 "Rawhide"      )
---      ( 50 "Hemming"      )
---      (  1  "Dreugh Wax"  )
+--      Item(100 "Rawhide")
+--      Item( 50 "Hemming")
+--      Item(  1  "Jute"  )
 --
 -- Suitable for MM-ification and multiplication (but that's someone else's focus).
 --
-function MMChat:ToLinkCounts(text)
+function MMChat:ToItemList(text)
     local r = {}
     for ct, link in string.gmatch(text, MMChat.LINK_PATTERN_N) do
         local item = Item:FromLinkCt(link, MMChat:ToInt(ct))
@@ -192,17 +168,6 @@ function MMChat.MMPrice(link)
     return mm.avgPrice
 end
 
-function MMChat:SendChatMessage(chat_event, message)
-    if chat_event.channel_id == CHAT_CHANNEL_WHISPER then
-        CHAT_SYSTEM:SetChannel(CHAT_CHANNEL_WHISPER, chat_event.from)
-    else
-        CHAT_SYSTEM:SetChannel(chat_event.channel_id)
-    end
-    d(message)
-    -- CHAT_SYSTEM:StartTextEntry(message)
-    -- CHAT_SYSTEM:SubmitTextEntry()
-end
-
 -- Init ----------------------------------------------------------------------
 
 function MMChat.OnAddOnLoaded(event, addonName)
@@ -213,7 +178,6 @@ function MMChat.OnAddOnLoaded(event, addonName)
 end
 
 function MMChat:Initialize()
-
     self.savedVariables = ZO_SavedVars:NewAccountWide(
                               "MMChatVars"
                             , self.savedVarVersion
@@ -231,7 +195,7 @@ function MMChat.AfterMessage()
     local event = table.remove(MMChat.text_queue, 1)
     if not event then return end
     local text = event.text
-    local item_list = MMChat:ToLinkCounts(text)
+    local item_list = MMChat:ToItemList(text)
     local total     = 0
     for i, item in ipairs(item_list) do
         item:FetchMM()
@@ -241,15 +205,9 @@ function MMChat.AfterMessage()
     local is_pc = MMChat:IsPriceCheck(text)
     local pc = ""
     if is_pc then
-        pc = "PRICE CHECK on channel " .. tostring(event.channel_id)
+        pc = "PRICE CHECK"
     end
     d(MMChat.ToMoney(total) .. "g total  " .. pc)
-    if is_pc then
-        for i, item in ipairs(item_list) do
-            local pc_text = item:PriceCheckText(item.link) .. " " ..  item:BoundHow()
-            MMChat:SendChatMessage(event, pc_text)
-        end
-    end
 end
 
 function MMChat.OnEventChatMessageChannel(
@@ -261,15 +219,12 @@ function MMChat.OnEventChatMessageChannel(
         )
     if not MMChat:IsEnabledForChannelID(channel_id) then return end
 
-    -- return text, zo_channel_info.saveTarget
     if MMChat:ContainsLink(text) and not MMChat:IsMMReport(text) then
         local event = ChatEvent:New(channel_id, from, text)
         table.insert(MMChat.text_queue, event)
         zo_callLater(MMChat.AfterMessage, 50)
-        -- MMChat.AfterMessage()
     end
 end
-
 
 -- Postamble -----------------------------------------------------------------
 
