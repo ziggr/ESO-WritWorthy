c76e33e6a433a50ce40de5f50bf4175d12c93baa
Zig Zichterman <ziggr@ziggr.com>
2016-10-24 17:48:43 -0700

    data

---
+++ data/MMChat.lua
@@ -0,0 +1,30 @@
+MMChatVars =
+{
+    ["Default"] = 
+    {
+        ["@ziggr"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["bag"] = 
+                {
+                },
+                ["channel_enabled"] = 
+                {
+                    [16] = false,
+                    [17] = false,
+                    [18] = false,
+                    [19] = true,
+                    [20] = true,
+                    [21] = false,
+                    [2] = true,
+                    [12] = false,
+                    [13] = false,
+                    [14] = true,
+                    [15] = true,
+                },
+                ["version"] = 1,
+            },
+        },
+    },
+}
