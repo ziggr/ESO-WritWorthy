8d41d4b2ead4b49f57210f78a042c07814fd3a39
Zig Zichterman <ziggr@ziggr.com>
2017-05-22 11:28:49 -0700

    Update API to 19/3.0 Morrowind

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -7,7 +7,7 @@ local WritWorthy = _G['WritWorthy'] -- defined in WritWorthy_Define.lua
 local LAM2 = LibStub("LibAddonMenu-2.0")
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "2.7.10"
+WritWorthy.version         = "3.0.1"
 WritWorthy.savedVarVersion = 1
 WritWorthy.default = {
     enable_mat_list_chat = false
