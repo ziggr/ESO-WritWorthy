743d47c20965ba8d90782096185736ccea871598
Zig Zichterman <github@ziggr.com>
2019-04-16 14:40:49 -0700

    publish 5.0.1  Update 22/5.0.0/API 100027/Elsweyr
    * Update 22/5.0.0/API 100027/Elsweyr
    * Use Baertram's LibSets for set info (number of traits required for a requested set). Optional. If LibSets isn't loaded, WritWorthy still has its own table of the same data.

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -7,7 +7,7 @@ local WritWorthy = _G['WritWorthy'] -- defined in WritWorthy_Define.lua
 local LAM2 = LibStub("LibAddonMenu-2.0")
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "4.3.3"
+WritWorthy.version         = "5.0.1"
 WritWorthy.savedVarVersion = 1
 
 -- Constants for savedVariables.enable_mat_list_chat
