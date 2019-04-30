52196e94704b074dd9bf68db12382d258c93a6e5
Zig Zichterman <github@ziggr.com>
2019-04-30 12:14:01 -0700

    publish 5.0.3
    * DE German translation, made _Better By Baertram._ Thank you!

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -8,7 +8,7 @@ local WW = WritWorthy
 local LAM2 = LibStub("LibAddonMenu-2.0")
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "5.0.2"
+WritWorthy.version         = "5.0.3"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
