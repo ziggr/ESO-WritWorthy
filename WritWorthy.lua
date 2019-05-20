cb9b6e4e59ccc41c5770084b1b5dcad8f4659d21
Zig Zichterman <github@ziggr.com>
2019-05-20 14:47:56 -0700

    publish 5.0.6
    * Refresh: clear cached skills. Refreshing the main WritWorthy window will now update to reflect any newly purchased skills such as Brewer or Temper Expertise. Thanks, Dolgubon, for the bug report.

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -8,7 +8,7 @@ local WW = WritWorthy
 local LAM2 = LibStub("LibAddonMenu-2.0")
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "5.0.5"
+WritWorthy.version         = "5.0.6"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
