5c120de18492a69b193de975a5b15902861a103f
Zig Zichterman <github@ziggr.com>
2019-11-06 14:30:35 -0800

    publish 5.2.2
    * "Max gold per voucher" filter disables/reds expensive writs that you don't want to craft. Thank you, Dolgubon!
    * Show writ counts in summary at bottom of window. Helps me avoid queuing more items than I have bag space to hold.
    * Setting "Use LibPrice for mat prices" lets you skip LibPrice/MM/ATT/TTC and go straight to the hardwired price table in WritWorthy_Price.lua.

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -8,7 +8,7 @@ local WW = WritWorthy
 local LAM2 = LibStub("LibAddonMenu-2.0")
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "5.2.1"
+WritWorthy.version         = "5.2.2"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
