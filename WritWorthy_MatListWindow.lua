2f01e8f95dea25fbb7b43e7dce531369b13b2296
Zig Zichterman <github@ziggr.com>
2019-12-18 11:24:56 -0800

    also wip

--- WritWorthy_MatListWindow.lua
+++ WritWorthy_MatListWindow.lua
@@ -16,3 +16,13 @@ function WritWorthy.MLToggle()
     local h = WritWorthyMatListUI:IsHidden()
     WritWorthyMatListUI:SetHidden(not h)
 end
+
+function WritWorthy.MLOnComboBoxChoice(choice)
+    Log.Debug("MLOnComboBoxChoice %s",tostring(choice))
+end
+
+function WritWorthy.MLInitComboBox(control)
+    Log.Debug("MLInitComboBox")
+    local cb_item = ZO_ComboBox:CreateItemEntry("All", WritWorthy.MLOnComboBoxChoice)
+    control:AddItem(cb_item)
+end
