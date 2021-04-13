08395b0c465de951dd8c5faa4af2596d0c1c965f
Zig Zichterman <github@ziggr.com>
2021-04-13 13:49:25 -0700

    prep for LibMotif-generated table
    Replace long-abandoned ITEMSTYLE_xxx named constants with numeric motif_id values.

--- WritWorthy_Smithing.lua
+++ WritWorthy_Smithing.lua
@@ -194,123 +194,126 @@ Smithing.TRAITS_JEWELRY = {
 --             IDs from CraftStore Fixed and Improved.
 --
 Smithing.MOTIF = {
-    [ITEMSTYLE_RACIAL_BRETON            ] = { is_simple =  true } -- Breton
-,   [ITEMSTYLE_RACIAL_REDGUARD          ] = { is_simple =  true } -- Redguard
-,   [ITEMSTYLE_RACIAL_ORC               ] = { is_simple =  true } -- Orc
-,   [ITEMSTYLE_RACIAL_DARK_ELF          ] = { is_simple =  true } -- Dunmer
-,   [ITEMSTYLE_RACIAL_NORD              ] = { is_simple =  true } -- Nord
-,   [ITEMSTYLE_RACIAL_ARGONIAN          ] = { is_simple =  true } -- Argonian
-,   [ITEMSTYLE_RACIAL_HIGH_ELF          ] = { is_simple =  true } -- Altmer
-,   [ITEMSTYLE_RACIAL_WOOD_ELF          ] = { is_simple =  true } -- Bosmer
-,   [ITEMSTYLE_RACIAL_KHAJIIT           ] = { is_simple =  true } -- Khajiit
-,   [ITEMSTYLE_UNIQUE                   ] = nil                   -- Unique
-,   [ITEMSTYLE_ORG_THIEVES_GUILD        ] = { pages_id  =  1423 } -- Thieves Guild
-,   [ITEMSTYLE_ORG_DARK_BROTHERHOOD     ] = { pages_id  =  1661 } -- Dark Brotherhood
-,   [ITEMSTYLE_DEITY_MALACATH           ] = { pages_id  =  1412 } -- Malacath
-,   [ITEMSTYLE_AREA_DWEMER              ] = { pages_id  =  1144 } -- Dwemer
-,   [ITEMSTYLE_AREA_ANCIENT_ELF         ] = { is_simple =  true } -- Ancient Elf
-,   [ITEMSTYLE_DEITY_AKATOSH            ] = { pages_id  =  1660 } -- Order of the Hour
-,   [ITEMSTYLE_AREA_REACH               ] = { is_simple =  true } -- Barbaric
-,   [ITEMSTYLE_ENEMY_BANDIT             ] = nil                   -- Bandit
-,   [ITEMSTYLE_ENEMY_PRIMITIVE          ] = { is_simple =  true } -- Primal
-,   [ITEMSTYLE_ENEMY_DAEDRIC            ] = { is_simple =  true } -- Daedric
-,   [ITEMSTYLE_DEITY_TRINIMAC           ] = { pages_id  =  1411 } -- Trinimac
-,   [ITEMSTYLE_AREA_ANCIENT_ORC         ] = { pages_id  =  1341 } -- Ancient Orc
-,   [ITEMSTYLE_ALLIANCE_DAGGERFALL      ] = { pages_id  =  1416 } -- Daggerfall Covenant
-,   [ITEMSTYLE_ALLIANCE_EBONHEART       ] = { pages_id  =  1414 } -- Ebonheart Pact
-,   [ITEMSTYLE_ALLIANCE_ALDMERI         ] = { pages_id  =  1415 } -- Aldmeri Dominion
-,   [ITEMSTYLE_UNDAUNTED                ] = { pages_id  =  1348 } -- Mercenary
-,   [ITEMSTYLE_RAIDS_CRAGLORN           ] = { pages_id  =  1714 } -- Celestial
-,   [ITEMSTYLE_GLASS                    ] = { pages_id  =  1319 } -- Glass
-,   [ITEMSTYLE_AREA_XIVKYN              ] = { pages_id  =  1181 } -- Xivkyn
-,   [ITEMSTYLE_AREA_SOUL_SHRIVEN        ] = { is_simple =  true } -- Soul-Shriven
-,   [ITEMSTYLE_ENEMY_DRAUGR             ] = { pages_id  =  1715 } -- Draugr
-,   [ITEMSTYLE_ENEMY_MAORMER            ] = nil                   -- Maormer
-,   [ITEMSTYLE_AREA_AKAVIRI             ] = { pages_id  =  1318 } -- Akaviri
-,   [ITEMSTYLE_RACIAL_IMPERIAL          ] = { is_simple =  true } -- Imperial
-,   [ITEMSTYLE_AREA_YOKUDAN             ] = { pages_id  =  1713 } -- Yokudan
-,   [ITEMSTYLE_UNIVERSAL                ] = nil                   -- Universal
-,   [ITEMSTYLE_AREA_REACH_WINTER        ] = nil                   -- Reach Winter
-,   [ITEMSTYLE_AREA_TSAESCI             ] = { crown_id  =132532 } -- Taesci
-,   [ITEMSTYLE_ENEMY_MINOTAUR           ] = { pages_id  =  1662 } -- Minotaur
-,   [ITEMSTYLE_EBONY                    ] = { pages_id  =  1798 } -- Ebony
-,   [ITEMSTYLE_ORG_ABAHS_WATCH          ] = { pages_id  =  1422 } -- Abah's Watch
-,   [ITEMSTYLE_HOLIDAY_SKINCHANGER      ] = { pages_id  =  1676 } -- Skinchanger
-,   [ITEMSTYLE_ORG_MORAG_TONG           ] = { pages_id  =  1933 } -- Morag Tong
-,   [ITEMSTYLE_AREA_RA_GADA             ] = { pages_id  =  1797 } -- Ra Gada
-,   [ITEMSTYLE_ENEMY_DROMOTHRA          ] = { pages_id  =  1659 } -- Dro-m'Athra
-,   [ITEMSTYLE_ORG_ASSASSINS            ] = { pages_id  =  1424 } -- Assassins League
-,   [ITEMSTYLE_ORG_OUTLAW               ] = { pages_id  =  1417 } -- Outlaw
-,   [ITEMSTYLE_ORG_REDORAN              ] = { pages_id  =  2022 } -- Redoran
-,   [ITEMSTYLE_ORG_HLAALU               ] = { pages_id  =  2021 } -- Hlaalu
-,   [ITEMSTYLE_ORG_ORDINATOR            ] = { pages_id  =  1935 } -- Militant Ordinator
-,   [ITEMSTYLE_ORG_TELVANNI             ] = { pages_id  =  2023 } -- Telvanni
-,   [ITEMSTYLE_ORG_BUOYANT_ARMIGER      ] = { pages_id  =  1934 } -- Buoyant Armiger
-,   [ITEMSTYLE_HOLIDAY_FROSTCASTER      ] = { crown_id  = 96954 } -- Stalhrim Frostcaster
-,   [ITEMSTYLE_AREA_ASHLANDER           ] = { pages_id  =  1932 } -- Ashlander
-,   [ITEMSTYLE_ORG_WORM_CULT            ] = { pages_id  =  2120 } -- Worm Cult
-,   [ITEMSTYLE_ENEMY_SILKEN_RING        ] = { pages_id  =  1796 } -- Silken Ring
-,   [ITEMSTYLE_ENEMY_MAZZATUN           ] = { pages_id  =  1795 } -- Mazzatun
-,   [ITEMSTYLE_HOLIDAY_GRIM_HARLEQUIN   ] = { crown_id  = 82039 } -- Grim Harlequin
-,   [ITEMSTYLE_HOLIDAY_HOLLOWJACK       ] = { pages_id  =  1545 } -- Hollowjack
-    -- Hollowjack/59 was the last named ITEMSTYLE_X constant.
-    -- It's all numeric from here on out.
-,   [ 60 ] = { pages_id  =  2024 } -- Refabricated
-,   [ 61 ] = { pages_id  =  2098 } -- Bloodforge
-,   [ 62 ] = { pages_id  =  2097 } -- Dreadhorn
-,   [ 63 ] = nil                   -- ""
-,   [ 64 ] = nil                   -- ""
-,   [ 65 ] = { pages_id  =  2044 } -- Apostle
-,   [ 66 ] = { pages_id  =  2045 } -- Ebonshadow
-,   [ 67 ] = nil                   -- Undaunted
-,   [ 68 ] = nil                   -- ""
-,   [ 69 ] = { pages_id  =  2190 } -- Fang Lair
-,   [ 70 ] = { pages_id  =  2189 } -- Scalecaller
-,   [ 71 ] = { pages_id  =  2186 } -- Psijic Order
-,   [ 72 ] = { pages_id  =  2187 } -- Sapiarch
-,   [ 73 ] = { pages_id  =  2319 } -- Welkynar
-,   [ 74 ] = { pages_id  =  2188 } -- Dremora
-,   [ 75 ] = { pages_id  =  2285 } -- Pyandonean
-,   [ 76 ] = nil                   -- Divine Prosecution
-,   [ 77 ] = { pages_id  =  2317 } -- Huntsman
-,   [ 78 ] = { pages_id  =  2318 } -- Silver Dawn
-,   [ 79 ] = { pages_id  =  2360 } -- Dead-Water
-,   [ 80 ] = { pages_id  =  2359 } -- Honor Guard
-,   [ 81 ] = { pages_id  =  2361 } -- Elder Argonian
-,   [ 82 ] = { pages_id  =  2503 } -- Coldsnap
-,   [ 83 ] = { pages_id  =  2504 } -- Meridian
-,   [ 84 ] = { pages_id  =  2505 } -- Anequina
-,   [ 85 ] = { pages_id  =  2506 } -- Pellitine
-,   [ 86 ] = { pages_id  =  2507 } -- Sunspire
-,   [ 87 ] = nil                   -- Dragon Bone
-,   [ 88 ] = nil                   -- ""
-,   [ 89 ] = { pages_id  =  2629 } -- Stags of Z'en
-,   [ 90 ] = nil                   -- ""
-,   [ 91 ] = nil                   -- ""
-,   [ 92 ] = { pages_id  =  2630 } -- Dragonguard
-,   [ 93 ] = { pages_id  =  2628 } -- Moongrave Fane
-,   [ 94 ] = { pages_id  =  2748 } -- New Moon Priest
-,   [ 95 ] = { pages_id  =  2750 } -- Shield of Senchal
-,   [ 96 ] = nil                   -- ""
-,   [ 97 ] = { pages_id  =  2747 } -- Icereach Coven
-,   [ 98 ] = { pages_id  =  2749 } -- Pyre Witch
-,   [ 99 ] = nil                   -- Swordthane
-,   [ 100 ] = { pages_id  =  2757 } -- Blackreach Vanguard
-,   [ 101 ] = { pages_id  =  2761 } -- Greymoor
-,   [ 102 ] = { pages_id  =  2762 } -- Sea Giant
-,   [ 103 ] = { pages_id  =  2763 } -- Ancestral Nord
-,   [ 104 ] = { pages_id  =  2773 } -- Ancestral High Elf
-,   [ 105 ] = { pages_id  =  2776 } -- Ancestral Orc
-,   [ 106 ] = { pages_id  =  2849 } -- Thorn Legion         2020-11-02 Update 28/Markarth
-,   [ 107 ] = { pages_id  =  2850 } -- Hazardous Alchemy     
-,   [ 108 ] = nil                   -- ""
-,   [ 109 ] = nil                   -- ""
-,   [ 110 ] = { pages_id  =  2905 } -- Ancestral Reach
-,   [ 111 ] = nil                   -- Nighthollow  
-,   [ 112 ] = { pages_id  =  2938 } -- Arkthzand Armory     
-,   [ 113 ] = nil                   -- Wayward Guardian 
+    [  1] = { is_simple =  true } -- Breton
+,   [  2] = { is_simple =  true } -- Redguard
+,   [  3] = { is_simple =  true } -- Orc
+,   [  4] = { is_simple =  true } -- Dunmer
+,   [  5] = { is_simple =  true } -- Nord
+,   [  6] = { is_simple =  true } -- Argonian
+,   [  7] = { is_simple =  true } -- Altmer
+,   [  8] = { is_simple =  true } -- Bosmer
+,   [  9] = { is_simple =  true } -- Khajiit
+,   [ 10] = nil                   -- Unique
+,   [ 11] = { pages_id  =  1423 } -- Thieves Guild
+,   [ 12] = { pages_id  =  1661 } -- Dark Brotherhood
+,   [ 13] = { pages_id  =  1412 } -- Malacath
+,   [ 14] = { pages_id  =  1144 } -- Dwemer
+,   [ 15] = { is_simple =  true } -- Ancient Elf
+,   [ 16] = { pages_id  =  1660 } -- Order of the Hour
+,   [ 17] = { is_simple =  true } -- Barbaric
+,   [ 18] = nil                   -- Bandit
+,   [ 19] = { is_simple =  true } -- Primal
+,   [ 20] = { is_simple =  true } -- Daedric
+,   [ 21] = { pages_id  =  1411 } -- Trinimac
+,   [ 22] = { pages_id  =  1341 } -- Ancient Orc
+,   [ 23] = { pages_id  =  1416 } -- Daggerfall Covenant
+,   [ 24] = { pages_id  =  1414 } -- Ebonheart Pact
+,   [ 25] = { pages_id  =  1415 } -- Aldmeri Dominion
+,   [ 26] = { pages_id  =  1348 } -- Mercenary
+,   [ 27] = { pages_id  =  1714 } -- Celestial
+,   [ 28] = { pages_id  =  1319 } -- Glass
+,   [ 29] = { pages_id  =  1181 } -- Xivkyn
+,   [ 30] = { is_simple =  true } -- Soul-Shriven
+,   [ 31] = { pages_id  =  1715 } -- Draugr
+,   [ 32] = nil                   -- Maormer
+,   [ 33] = { pages_id  =  1318 } -- Akaviri
+,   [ 34] = { is_simple =  true } -- Imperial
+,   [ 35] = { pages_id  =  1713 } -- Yokudan
+,   [ 36] = nil                   -- Universal
+,   [ 37] = nil                   -- Reach Winter
+,   [ 38] = { crown_id  =132532 } -- Taesci
+,   [ 39] = { pages_id  =  1662 } -- Minotaur
+,   [ 40] = { pages_id  =  1798 } -- Ebony
+,   [ 41] = { pages_id  =  1422 } -- Abah's Watch
+,   [ 42] = { pages_id  =  1676 } -- Skinchanger
+,   [ 43] = { pages_id  =  1933 } -- Morag Tong
+,   [ 44] = { pages_id  =  1797 } -- Ra Gada
+,   [ 45] = { pages_id  =  1659 } -- Dro-m'Athra
+,   [ 46] = { pages_id  =  1424 } -- Assassins League
+,   [ 47] = { pages_id  =  1417 } -- Outlaw
+,   [ 48] = { pages_id  =  2022 } -- Redoran
+,   [ 49] = { pages_id  =  2021 } -- Hlaalu
+,   [ 50] = { pages_id  =  1935 } -- Militant Ordinator
+,   [ 51] = { pages_id  =  2023 } -- Telvanni
+,   [ 52] = { pages_id  =  1934 } -- Buoyant Armiger
+,   [ 53] = { crown_id  = 96954 } -- Stalhrim Frostcaster
+,   [ 54] = { pages_id  =  1932 } -- Ashlander
+,   [ 55] = { pages_id  =  2120 } -- Worm Cult
+,   [ 56] = { pages_id  =  1796 } -- Silken Ring
+,   [ 57] = { pages_id  =  1795 } -- Mazzatun
+,   [ 58] = { crown_id  = 82039 } -- Grim Harlequin
+,   [ 59] = { pages_id  =  1545 } -- Hollowjack
+,   [ 60] = { pages_id  =  2024 } -- Refabricated
+,   [ 61] = { pages_id  =  2098 } -- Bloodforge
+,   [ 62] = { pages_id  =  2097 } -- Dreadhorn
+,   [ 63] = nil                   --
+,   [ 64] = nil                   --
+,   [ 65] = { pages_id  =  2044 } -- Apostle
+,   [ 66] = { pages_id  =  2045 } -- Ebonshadow
+,   [ 67] = nil                   -- Undaunted
+,   [ 68] = nil                   --
+,   [ 69] = { pages_id  =  2190 } -- Fang Lair
+,   [ 70] = { pages_id  =  2189 } -- Scalecaller
+,   [ 71] = { pages_id  =  2186 } -- Psijic Order
+,   [ 72] = { pages_id  =  2187 } -- Sapiarch
+,   [ 73] = { pages_id  =  2319 } -- Welkynar
+,   [ 74] = { pages_id  =  2188 } -- Dremora
+,   [ 75] = { pages_id  =  2285 } -- Pyandonean
+,   [ 76] = nil                   -- Divine Prosecution
+,   [ 77] = { pages_id  =  2317 } -- Huntsman
+,   [ 78] = { pages_id  =  2318 } -- Silver Dawn
+,   [ 79] = { pages_id  =  2360 } -- Dead-Water
+,   [ 80] = { pages_id  =  2359 } -- Honor Guard
+,   [ 81] = { pages_id  =  2361 } -- Elder Argonian
+,   [ 82] = { pages_id  =  2503 } -- Coldsnap
+,   [ 83] = { pages_id  =  2504 } -- Meridian
+,   [ 84] = { pages_id  =  2505 } -- Anequina
+,   [ 85] = { pages_id  =  2506 } -- Pellitine
+,   [ 86] = { pages_id  =  2507 } -- Sunspire
+,   [ 87] = nil                   -- Dragon Bone
+,   [ 88] = nil                   --
+,   [ 89] = { pages_id  =  2629 } -- Stags of Z'en
+,   [ 90] = nil                   --
+,   [ 91] = nil                   --
+,   [ 92] = { pages_id  =  2630 } -- Dragonguard
+,   [ 93] = { pages_id  =  2628 } -- Moongrave Fane
+,   [ 94] = { pages_id  =  2748 } -- New Moon Priest
+,   [ 95] = { pages_id  =  2750 } -- Shield of Senchal
+,   [ 96] = nil                   --
+,   [ 97] = { pages_id  =  2747 } -- Icereach Coven
+,   [ 98] = { pages_id  =  2749 } -- Pyre Witch
+,   [ 99] = nil                   -- Swordthane
+,   [100] = { pages_id  =  2757 } -- Blackreach Vanguard
+,   [101] = { pages_id  =  2761 } -- Greymoor
+,   [102] = { pages_id  =  2762 } -- Sea Giant
+,   [103] = { pages_id  =  2763 } -- Ancestral Nord
+,   [104] = { pages_id  =  2773 } -- Ancestral High Elf
+,   [105] = { pages_id  =  2776 } -- Ancestral Orc
+,   [106] = { pages_id  =  2849 } -- Thorn Legion
+,   [107] = { pages_id  =  2850 } -- Hazardous Alchemy
+,   [108] = nil                   --
+,   [109] = nil                   --
+,   [110] = { pages_id  =  2905 } -- Ancestral Reach
+,   [111] = nil                   -- Nighthollow
+,   [112] = { pages_id  =  2938 } -- Arkthzand Armory
+,   [113] = nil                   -- Wayward Guardian
+,   [114] = nil                   --
+,   [115] = nil                   --
+,   [116] = nil                   -- True-Sworn
+,   [117] = nil                   -- Waking Flame
 
---    113 is GetHighestItemStyleId() as of 2020-11-02 Markarth
+
+--    117 is GetHighestItemStyleId() as of 2021-04-13
 
 --,   [ 114 ] = { pages_id  =  questid } -- NewStyle
 
@@ -933,8 +936,8 @@ function Parser:ToKnowList()
         local llc = WritWorthyInventoryList:GetLLC()
         if llc and self.set_bonus.set_id then
             local t = llc.GetSetIndexes()
-            local llc_can_craft = self.set_bonus.set_id 
-                                  and t 
+            local llc_can_craft = self.set_bonus.set_id
+                                  and t
                                   and (t[self.set_bonus.set_id] ~= nil)
             local msg = string.format( WritWorthy.Str( "know_err_llc_too_old")
                                                      , tostring(llc.version)
