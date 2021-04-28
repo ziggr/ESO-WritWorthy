f2c9455e868f10e82583d751db92bd7b34be1cd8
Zig Zichterman <github@ziggr.com>
2021-04-27 21:04:38 -0700

    publish 7.0.1
    * Update 30/ESO 7.0.0/API 100035/Blackwood
    * new and experimental: /writworthy mat: display a list of required (or just missing) materials

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -8,7 +8,7 @@ local WW = WritWorthy
 local LAM2 = LibAddonMenu2
 
 WritWorthy.name            = "WritWorthy"
-WritWorthy.version         = "6.3.1"
+WritWorthy.version         = "7.0.1"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
